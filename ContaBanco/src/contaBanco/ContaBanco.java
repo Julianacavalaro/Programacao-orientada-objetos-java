4bd4e7c8c430d618118e49d40a14adbc9b8facbc
juliana <juliana.cavalaro@hotmail.com>
2020-11-22 14:46:55 -0300

    criação dos métodos

--- ContaBanco/src/contaBanco/ContaBanco.java
+++ ContaBanco/src/contaBanco/ContaBanco.java
@@ -7,45 +7,89 @@ public class ContaBanco {
 	private String dono;
 	private double saldo;
 	private boolean status;
-	
+
 	public void abrirConta() {
 		this.status = true;
 	}
+
 	public void fecharConta() {
-		this.status = false;
+		if (this.status == true) {
+			this.status = false;
+		} else {
+			System.out.println("Sua conta já não se encontra aberta");
+		}
 	}
-	//getters e setters
+
+	public void depositar(double depositar) {
+		this.saldo = saldo + depositar;
+	}
+
+	public void sacar(double sacar) {
+		if (this.saldo <= 0) {
+			System.out.println("Não é possível sacar com saldo menor que zero");
+		} else {
+			this.saldo = saldo - sacar;
+		}
+	}
+
+	public void fecharConta(double sacar) {
+		if (this.status == true) {
+			this.status = false;
+			System.out.println("Conta fechada com sucesso");
+		} else {
+			System.out.println("Sua conta já está fechada");
+		}
+
+	}
+	public void pagarMensalidade(double mensalidade) {
+		if (this.saldo <= 0) {
+			System.out.println("Saldo insuficiente para pagar a mensalidade");
+		}
+		this.saldo = saldo - mensalidade;
+	}
+
+	// getters e setters
 	public int getNumConta() {
 		return numConta;
 	}
+
 	public void setNumConta(int numConta) {
 		this.numConta = numConta;
 	}
+
 	public String getTipo() {
 		return tipo;
 	}
+
 	public void setTipo(String tipo) {
 		this.tipo = tipo;
 	}
+
 	public String getDono() {
 		return dono;
 	}
+
 	public void setDono(String dono) {
 		this.dono = dono;
 	}
+
 	public double getSaldo() {
 		return saldo;
 	}
+
 	public void setSaldo(double saldo) {
 		this.saldo = saldo;
 	}
+
 	public boolean isStatus() {
 		return status;
 	}
+
 	public void setStatus(boolean status) {
 		this.status = status;
 	}
-	//contrutores
+
+	// contrutores
 	public ContaBanco(int numConta, String tipo, String dono, double saldo, boolean status) {
 		super();
 		this.numConta = numConta;
@@ -54,6 +98,5 @@ public class ContaBanco {
 		this.saldo = saldo;
 		this.status = status;
 	}
-	
-	
+
 }
