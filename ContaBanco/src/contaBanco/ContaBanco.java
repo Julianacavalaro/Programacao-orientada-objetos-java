a544fec5c9876a9a774ddd4e3655af239d15909d
juliana <juliana.cavalaro@hotmail.com>
2020-11-22 15:55:15 -0300

    métodos específicos

--- ContaBanco/src/contaBanco/ContaBanco.java
+++ ContaBanco/src/contaBanco/ContaBanco.java
@@ -2,83 +2,44 @@ package contaBanco;
 
 public class ContaBanco {
 
+	// atributos
 	public int numConta;
 	protected String tipo;
 	private String dono;
 	private double saldo;
 	private boolean status;
 
-	public void abrirConta() {
-		this.status = true;
-	}
-
-	public void fecharConta() {
-		if (this.status == true) {
-			this.status = false;
-		} else {
-			System.out.println("Sua conta já não se encontra aberta");
-		}
-	}
-
-	public void depositar(double depositar) {
-		this.saldo = saldo + depositar;
-	}
-
-	public void sacar(double sacar) {
-		if (this.saldo <= 0) {
-			System.out.println("Não é possível sacar com saldo menor que zero");
-		} else {
-			this.saldo = saldo - sacar;
-		}
-	}
-
-	public void fecharConta(double sacar) {
-		if (this.status == true) {
-			this.status = false;
-			System.out.println("Conta fechada com sucesso");
-		} else {
-			System.out.println("Sua conta já está fechada");
-		}
-
-	}
-	public void pagarMensalidade(double mensalidade) {
-		if (this.saldo <= 0) {
-			System.out.println("Saldo insuficiente para pagar a mensalidade");
-		}
-		this.saldo = saldo - mensalidade;
-	}
-
 	// getters e setters
 	public int getNumConta() {
 		return numConta;
 	}
 
-	public void setNumConta(int numConta) {
-		this.numConta = numConta;
+	public void setNumConta(int n) {
+		this.numConta = n;
 	}
 
 	public String getTipo() {
 		return tipo;
 	}
 
-	public void setTipo(String tipo) {
-		this.tipo = tipo;
+	public void setTipo(String t) {
+		this.tipo = t;
 	}
 
 	public String getDono() {
 		return dono;
 	}
 
-	public void setDono(String dono) {
-		this.dono = dono;
+	public void setDono(String d) {
+		this.dono = d;
 	}
 
 	public double getSaldo() {
 		return saldo;
 	}
 
-	public void setSaldo(double saldo) {
-		this.saldo = saldo;
+	public void setSaldo(double s) {
+		this.saldo = s;
 	}
 
 	public boolean isStatus() {
@@ -89,7 +50,12 @@ public class ContaBanco {
 		this.status = status;
 	}
 
-	// contrutores
+	// Métodos especiais - contrutores
+	public void Construtor() {
+		setSaldo(0);
+		setStatus(false);
+	}
+
 	public ContaBanco(int numConta, String tipo, String dono, double saldo, boolean status) {
 		super();
 		this.numConta = numConta;
@@ -97,6 +63,70 @@ public class ContaBanco {
 		this.dono = dono;
 		this.saldo = saldo;
 		this.status = status;
+	}
+
+	// métodos específicos (primeiro fazer depois dos outros método)
+	public void abrirConta(String t) {
+		setTipo(t);
+		setStatus(true);
+		if (t == "cc") {
+			this.saldo = saldo + 50;
+		} else if (t == "cp") {
+			this.saldo = saldo + 150;
+		}
+	}
+
+	public void fecharConta() {
+		if (this.getSaldo() > 0) {
+			System.out.println("impossível fechar conta com dinheiro");
+		} else if (this.getSaldo() < 0) {
+			System.out.println("impossível fechar com débito");
+		} else {
+			setStatus(false);
+			System.out.println("Conta fechada com sucesso");
+		}
+	}
+
+	public void depositar(double v) {
+		if (status == true) {
+			setSaldo(getSaldo() + v);
+			this.setSaldo(this.getSaldo() + v);
+			System.out.println("depósito realizado com sucesso");
+		} else {
+			System.out.println("Impossível depositar com a conta fechada");
+		}
+	}
+
+	public void sacar(double v) {
+		if (status == true) {
+			if (this.saldo > 0) {
+				setSaldo(getSaldo()- v);
+				
+			} else {
+				System.out.println("Não é possível sacar com saldo menor que zero");
+			}
+		}else {
+			System.out.println("Impossível sacar com a conta inativa");
+		}
+
+	}
+
+
+	public void pagarMensalidade() {
+		int v =0;
+		if (tipo=="cc") {
+			v = 12;
+		}
+		else if (tipo == "cp") {
+			v =20;
+		}
+		if (status == true) {
+			if(saldo > 0) {
+				setSaldo(getSaldo() - v);
+			}else {
+				System.out.println("Impossível pagar");
+			}
+		}
 	}
 
 }
